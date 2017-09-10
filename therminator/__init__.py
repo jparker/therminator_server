1551d7fb2d3db13eda93f48d2aad650ed008447a
John Parker <jparker@urgetopunt.com>
2017-09-10 09:29:21 -0700

    refresh login config

--- therminator/__init__.py
+++ therminator/__init__.py
@@ -24,6 +24,8 @@ login_manager.init_app(app)
 login_manager.login_view = 'sign_in'
 login_manager.login_message = u'You must sign in before proceeding.'
 login_manager.login_message_category = 'info'
+login_manager.refresh_view = 'sign_in'
+login_manager.needs_refresh_message = u'You must reauthenticate again before proceeding.'
 login_manager.session_protection = 'basic'
 
 sslify = SSLify(app, permanent=True)
